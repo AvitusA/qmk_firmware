7bcf50ef7cfdd414ba380c09d0269c873cbd422a
Fredrik Nilsson <fredrik.nilsson@izettle.com>
2020-05-03 18:26:41 +0200

    mod tap space and enter

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/AvitusA/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/AvitusA/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                      KC_LCTL,KC_LALT,                                                        KC_RALT,KC_RCTL,
-                                      LOWER , RAISE ,                         KC_ENT, KC_SPC,
+                                      LOWER , RAISE ,                         LALT_T(KC_ENT), LCTL_T(KC_SPC),
                                       KC_NO , KC_NO,                          GREEK , KC_RGUI,
                                       KC_ESC, KC_NO ,                         KC_NO , KC_MINS
   ),
